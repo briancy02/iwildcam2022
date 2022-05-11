254ba7f374b0c6a40c361914701f325f63274131
Brian Yoon <byoon2002@gmail.com>
2022-05-11 00:00:17 -0500

    fixed linear assignment bug

--- deep_sort/deep_sort/tracker.py
+++ deep_sort/deep_sort/tracker.py
@@ -67,6 +67,9 @@ class Tracker:
         # Run matching cascade.
         matches, unmatched_tracks, unmatched_detections = \
             self._match(detections)
+        print(matches)
+        print(unmatched_tracks)
+        print(unmatched_detections)
 
         # Update track set.
         for track_idx, detection_idx in matches:
